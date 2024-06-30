f2dc1fc95895d4aea23deb54da25af0da15db325
bobetbat <b.bobetayev.97@gmail.com>
2024-06-30 15:13:39 +0200

    fix: window error

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -5,6 +5,9 @@ type SetValue<T> = (value: T | ((val: T) => T)) => void;
 const useLocalStorage = <T,>(key: string, initialValue: T | null = null): [T | null, SetValue<T>] => {
   // Function to safely parse JSON stored in localStorage
   const readValue = (): T | null => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
     try {
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
@@ -30,12 +33,16 @@ const useLocalStorage = <T,>(key: string, initialValue: T | null = null): [T | n
   }, [key, initialValue]);
 
   const setValue: SetValue<T> = value => {
-    try {
-      const valueToStore = value instanceof Function ? value(storedValue as T) : value;
-      setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-      console.warn(`Error setting localStorage key “${key}”:`, error);
+    if (typeof window !== 'undefined') {
+      try {
+        const valueToStore = value instanceof Function ? value(storedValue as T) : value;
+        setStoredValue(valueToStore);
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      } catch (error) {
+        console.warn(`Error setting localStorage key “${key}”:`, error);
+      }
+    } else {
+      console.warn(`window is not defined. Cannot set localStorage key “${key}”.`);
     }
   };
 
