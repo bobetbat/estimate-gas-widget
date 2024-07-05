3301dce4356b5eb5cd3b7068c14ea3137d86c064
bobetbat <b.bobetayev.97@gmail.com>
2024-07-05 15:37:12 +0200

    fix: exports

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider, CssBaseline, Theme } from '@mui/material';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { Config, WagmiProvider } from 'wagmi';
-
 import { config } from './config/wagmi';
 import { getTheme } from './config/theme';
 import useLocalStorage from './hooks/useLocalStorage';
